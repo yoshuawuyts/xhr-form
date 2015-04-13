66217ba9f3e1d095bb32ebf51de7d23051848326
Yoshua Wuyts <i@yoshuawuyts.com>
2015-04-12 23:54:06 -0700

    xhr-form: fix broken file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const customElement = require('custom-element')
 const event = require('synthetic-dom-events')
-const emitter = require('component-emitter')
 const sliced = require('sliced')
 
 const form = customElement(window.HTMLFormElement.prototype)
@@ -9,20 +8,23 @@ form.extends = 'form'
 module.exports = form
 
 form.on('created', function () {
-  this.addEventListener('submit', )
+  this.addEventListener('submit', this.listener = listener.bind(this))
+  function listener (e) {
+    e.stopPropagation()
+    e.preventDefault()
+
+    const els = e.target.elements || []
+    const res = {}
+
+    sliced(els).forEach(function (el) {
+      const name = el.name
+      if (name) res[name] = el.value
+    })
+
+    this.dispatchEvent(event('xhr', {data: res}))
+  }
 })
 
-function listener (e) {
-  e.stopPropagation()
-  e.preventDefault()
-
-  const els = e.target.elements || {}
-  const res = {}
-
-  sliced(els).forEach(function(el) {
-    const name = el.name
-    if (name) res[name] = el.value
-  })
-
-  this.dispatchEvent(event('xhr', {data: res}))
-}.bind(this)
+form.on('detached', function () {
+  this.removeEventListener('submit', this.listener)
+})
